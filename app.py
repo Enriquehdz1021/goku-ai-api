722ea5a29d4632bbd85f09783707c6205e196e73
Enriquehdz1021 <hilarionhernandez1991@gmail.com>
2025-03-08 04:57:45 -0800

    Update app.py
    Fixed API endpoint for number recognition.
    Updated model path in app.py
    Added error handling for missing images.
    Improve response format for predictions.

--- app.py
+++ app.py
@@ -23,4 +23,6 @@ def predict():
     return jsonify({"Predicted Number": int(prediction)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render usa la variable PORT
+    app.run(host="0.0.0.0", port=port)
